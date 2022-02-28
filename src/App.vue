53ae0401ee2829efe65555ec2e6a6e353cffcc17
big-monty <amp85@mac.com>
2022-02-28 14:54:30 -0600

    remake project properly

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import '@/assets/base.css';
+
+#app {
+}
+
+</style>
